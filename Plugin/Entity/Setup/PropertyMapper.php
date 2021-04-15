d8cd58ab5ef244c3420bd3240b86ad5aeca3819e
Kristian Ernst <kernst@mediarox.de>
2021-04-15 11:38:59 +0200

    [*] refactor code

--- Plugin/Entity/Setup/PropertyMapper.php
+++ Plugin/Entity/Setup/PropertyMapper.php
@@ -9,6 +9,20 @@ namespace Mediarox\EavPropertyMapper\Plugin\Entity\Setup;
 
 class PropertyMapper
 {
+    protected const EAV_PROPERTIES_LONG_TO_SHORT = [
+        'backend_type' => 'type',
+        'backend_model' => 'backend',
+        'backend_table'=> 'table',
+        'frontend_model'=> 'frontend',
+        'frontend_input'=> 'input',
+        'frontend_label'=> 'label',
+        'source_model'=> 'source',
+        'is_required'=> 'required',
+        'is_user_defined'=> 'user_defined',
+        'default_value'=> 'default',
+        'is_unique'=> 'unique',
+        'is_global'=> 'global'
+    ];
     /**
      * Map input attribute properties to storage representation
      *
@@ -20,45 +34,14 @@ class PropertyMapper
     public function beforeMap($subject, array $input, int $entityTypeId)
     {
         foreach ($input as $key => $value) {
-            $input = $this->check($input, $key);
+            if(array_key_exists($key, self::EAV_PROPERTIES_LONG_TO_SHORT))
+            {
+                $shortProperty = self::EAV_PROPERTIES_LONG_TO_SHORT[$key];
+                $input[$shortProperty] = $value;
+                unset($input[$key]);
+            }
         }
 
         return [$input, $entityTypeId];
-    }
-
-    private function check($array, $key){
-        switch ($key){
-            case'backend_model': $newKey = 'backend';
-                break;
-            case'backend_type': $newKey = 'type';
-                break;
-            case'backend_table': $newKey = 'table';
-                break;
-            case'frontend_model': $newKey = 'frontend';
-                break;
-            case'frontend_input': $newKey = 'input';
-                break;
-            case'frontend_label': $newKey = 'label';
-                break;
-            case'source_model': $newKey = 'source';
-                break;
-            case'is_required': $newKey = 'required';
-                break;
-            case'is_user_defined': $newKey = 'user_defined';
-                break;
-            case'default_value': $newKey = 'default';
-                break;
-            case'is_unique': $newKey = 'unique';
-                break;
-            case'is_global': $newKey = 'global';
-                break;
-            default: $newKey = $key;
-        }
-
-        if(array_key_exists($key, $array)) {
-            $array[$newKey] = $array[$key];
-            unset($array[$key]);
-        }
-        return $array;
     }
 }
